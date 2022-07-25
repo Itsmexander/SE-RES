214f0fc658405b35ad912181ba62006307c47149
Itsmexander <chawish.p@ku.th>
2022-07-25 13:31:10 +0700

    Add GET, PUT, DELETE related methods

--- menu/src/main/java/th/ac/ku/menu/service/MenuService.java
+++ menu/src/main/java/th/ac/ku/menu/service/MenuService.java
@@ -6,6 +6,7 @@ import th.ac.ku.menu.model.Menu;
 import th.ac.ku.menu.repository.MenuRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MenuService {
@@ -20,5 +21,34 @@ public class MenuService {
     public Menu create(Menu menu){
         Menu record = menuRepository.save(menu);
         return record;
+    }
+
+    public Menu getMenuById (UUID id){
+        return menuRepository.findById(id).get();
+    }
+
+    public Menu update(Menu requestBody){
+        UUID id = requestBody.getId();
+        Menu record = menuRepository.findById(id).get();
+        record.setName(requestBody.getName());
+        record.setPrice(requestBody.getPrice());
+        record.setCategory(record.getCategory());
+
+        record = menuRepository.save(record);
+        return record;
+    }
+
+    public Menu delete(UUID id){
+        Menu record = menuRepository.findById(id).get();
+        menuRepository.deleteById(id);
+        return record;
+    }
+
+    public Menu getMenuByName(String name){
+        return menuRepository.findByName(name);
+    }
+
+    public List<Menu> getMenuByCategory(String category){
+        return menuRepository.findByCategory(category);
     }
 }
